a123b10462dc53309a8eda44409ddabf2412d77b
kagome <fu.taz.1102@gmail.com>
2024-04-28 14:06:10 +0900

    feat add timer #4

---
+++ js/event.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const counter = document.getElementById('count');
+let count = 0;
+
+const timer = document.getElementById('time');
+let startTime, elapsedTime;
+
+tiles.forEach(tile => {
+  tile.addEventListener('click', (e) => {
+    click(e);
+
+    // マス移動が失敗しなければカウンターを更新する
+    if (moveFailed == false) {
+
+      // はじめてのマス移動であればタイマーを始動する
+      if (count == 0) {
+        startTime = Date.now();
+        const timerId = setInterval(() => {
+          elapsedTime = Date.now() - startTime;
+          timer.textContent = Math.floor(elapsedTime / 1000);
+        }, 1000);
+
+        count++;
+        counter.textContent = count;
+      }
+    }
+
+    moveFailed = false;
+  })
+});
